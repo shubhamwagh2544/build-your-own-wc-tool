44090993013b30e428ac1ad84ff00aed7c9aecb9
Shubh <shubhamwagh2544@gmail.com>
2026-01-21 08:11:14 +0530

    wc -l <file> implemented

--- wc-js/src/jwc.js
+++ wc-js/src/jwc.js
@@ -5,38 +5,56 @@ import fs from 'node:fs';
 // parse cli arguments
 const args = process.argv.slice(2);
 
-if (args.length === 0 || args[0] !== '-c') {
-    console.error("Usage: ./jwc.js -c [file]")
-    process.exit(1)
+if (args.length === 0) {
+    console.error("Usage: wc [-c|-l] [file]");
+    process.exit(1);
+}
+
+const option = args[0];
+const filePath = args[1];
+
+if (option !== "-c" && option !== "-l") {
+    console.error("Usage: ccwc [-c|-l] [file]");
+    process.exit(1);
 }
 
 // input stream
-let stream;
-let filePath = args[1];
-if (filePath) {
-    try {
-        stream = fs.createReadStream(filePath)
-    } catch (err) {
-        console.error('Unexpected Error: ', err.message)
-    }
-} else {
-    stream = process.stdin;
-}
+let stream = filePath ? fs.createReadStream(filePath) : process.stdin;
 
 // process stream
 let byteCount = 0;
+let lineCount = 0;
+
 stream.on('data', (chunk) => {
-    // console.log(chunk)  // <Buffer 48 65 6c 6c 6f 20 57 6f 72 6c 64 21 0a>
-    byteCount += chunk.length
+    console.log(chunk)  // <Buffer 48 65 6c 6c 6f 20 57 6f 72 6c 64 21 0a>
+    byteCount += chunk.length;
+    if (option === '-l') {
+        for (let i = 0; i < chunk.length; i++) {
+            if (chunk[i] === 10) {  // 10 is ASCII for '\n'
+                lineCount++;
+            }
+        }
+    }
+
 })
 stream.on('error', (err) => {
     console.error('Unexpected Error: ', err.message)
     process.exit(1)
 })
-stream.on('end', () => {
-    if (filePath) {
-        console.log(`${byteCount} ${filePath}`)
+
+stream.on("end", () => {
+    let result;
+
+    if (option === "-c") {
+        result = byteCount;
     } else {
-        console.log(byteCount)
+        result = lineCount;
     }
-})
+
+    if (filePath) {
+        console.log(`${result} ${filePath}`);
+    }
+    else {
+        console.log(result);
+    }
+});
